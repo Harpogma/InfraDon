10fa70756d59a2a6b00ccf365c395994e40d1059
Harpogma <gd.temp@pm.me>
2025-05-01 09:55:04 +0200

    update all requests

--- scripts/fill_type_table.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO type (id, short_descr, long_descr)
-VALUES(1, 'Mai', 'Mail');
-
-INSERT INTO type (id, short_descr, long_descr)
-VALUES(2, 'Liv', 'Live');
-
-INSERT INTO type (id, short_descr, long_descr)
-VALUES(3, 'Pho', 'Phone');
-
